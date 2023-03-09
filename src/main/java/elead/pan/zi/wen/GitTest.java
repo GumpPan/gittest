c766463f2a0a093cf489e674f74e677888e13531
GumpPan <GumpPan@163.com>
2023-03-09 23:14:23 +0800

    hot-fix commit1

--- src/main/java/elead/pan/zi/wen/GitTest.java
+++ src/main/java/elead/pan/zi/wen/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("args = " + args);
         System.out.println("晚上好");
+        System.out.println("hot-fix");
     }
 }
