824e86c876cdd8c69351816ef71fe916498459a7
GumpPan <GumpPan@163.com>
2023-03-09 22:42:49 +0800

    first commit

---
+++ src/main/java/elead/pan/zi/wen/GitTest.java
@@ -0,0 +1,13 @@
+package elead.pan.zi.wen;
+
+/**
+ * elead.pan.zi.wen
+ *
+ * @author GumpPan
+ * @date 2023/3/9
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
