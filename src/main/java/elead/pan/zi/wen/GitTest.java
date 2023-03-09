9b5798cbe7ad2c901518ef17a156b5c809bd4a2a
GumpPan <GumpPan@163.com>
2023-03-09 22:44:34 +0800

    second commit

--- src/main/java/elead/pan/zi/wen/GitTest.java
+++ src/main/java/elead/pan/zi/wen/GitTest.java
@@ -9,5 +9,6 @@ package elead.pan.zi.wen;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+        System.out.println("晚上好");
     }
 }
