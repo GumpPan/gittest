4d761f122ad3d77e7d6ef242b3cdeba9bab99347
GumpPan <127311068+GumpPan@users.noreply.github.com>
2023-03-10 08:21:36 +0800

    Update GitTest.java
    pull test

--- src/main/java/elead/pan/zi/wen/GitTest.java
+++ src/main/java/elead/pan/zi/wen/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hot-fix 2");
         System.out.println("master commit1");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
