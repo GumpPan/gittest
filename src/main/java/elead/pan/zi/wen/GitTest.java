1d2a150d27164455805ef144f715add3d0bd8cdc
GumpPan <GumpPan@163.com>
2023-03-10 08:15:01 +0800

    push commit

--- src/main/java/elead/pan/zi/wen/GitTest.java
+++ src/main/java/elead/pan/zi/wen/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("hot-fix 2");
         System.out.println("master commit1");
+        System.out.println("push test");
     }
 }
